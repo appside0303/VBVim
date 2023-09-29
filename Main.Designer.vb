0c3ae40edda18259663b5c71910394576b2b29cd
appside0303 <nexus6user2023132432@gmail.com>
2023-09-29 13:57:11 +0900

    first commit

---
+++ Main.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.textArea1 = New System.Windows.Forms.TextBox()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.textArea2 = New System.Windows.Forms.TextBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.textArea1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SplitContainer2)
+        Me.SplitContainer1.Size = New System.Drawing.Size(477, 420)
+        Me.SplitContainer1.SplitterDistance = 364
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'textArea1
+        '
+        Me.textArea1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.textArea1.Location = New System.Drawing.Point(0, 0)
+        Me.textArea1.Multiline = True
+        Me.textArea1.Name = "textArea1"
+        Me.textArea1.Size = New System.Drawing.Size(477, 364)
+        Me.textArea1.TabIndex = 0
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.textArea2)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.StatusStrip1)
+        Me.SplitContainer2.Size = New System.Drawing.Size(477, 52)
+        Me.SplitContainer2.SplitterDistance = 25
+        Me.SplitContainer2.TabIndex = 0
+        '
+        'textArea2
+        '
+        Me.textArea2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.textArea2.Location = New System.Drawing.Point(0, 0)
+        Me.textArea2.Name = "textArea2"
+        Me.textArea2.Size = New System.Drawing.Size(477, 19)
+        Me.textArea2.TabIndex = 0
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 3)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(477, 22)
+        Me.StatusStrip1.TabIndex = 0
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 420)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Main"
+        Me.Text = "Vim"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.PerformLayout()
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.PerformLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents textArea1 As System.Windows.Forms.TextBox
+    Friend WithEvents SplitContainer2 As System.Windows.Forms.SplitContainer
+    Friend WithEvents textArea2 As System.Windows.Forms.TextBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Main.vb
@@ -0,0 +1,246 @@
+﻿Imports System.IO
+Imports System.Text
+
+'
+' Visual Basic.netでvimエディタを作成しよう
+'
+Public Class Main
+    Private mode As Integer = 0 '0:コマンドモード、1:入力モード
+    Private caretRow As Integer = 0
+    Private caretColumn As Integer = 0
+    Private changed As Boolean = False
+    Private filename As String = ""
+    Private commandBuffer As String = ""
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        textArea1.BackColor = Color.Black
+        textArea2.BackColor = Color.Black
+        textArea1.ForeColor = Color.White
+        textArea2.ForeColor = Color.White
+
+        StatusStrip1.Items(0).Text = ""
+
+        Dim args As String() = Environment.GetCommandLineArgs()
+
+        If args.Length = 2 Then
+            filename = args(1)
+
+            readTextfile(filename)
+
+            textArea1.SelectionStart = 0
+            textArea1.SelectionLength = 0
+
+        End If
+
+    End Sub
+
+    Private Sub textArea1_KeyDown(sender As Object, e As KeyEventArgs) Handles textArea1.KeyDown
+        Dim counter As Integer = 0
+        Timer1.Enabled = True
+
+        If e.KeyCode = Keys.Escape Then
+            Try
+                textArea1.SelectionStart -= 1
+            Catch ex As Exception
+            End Try
+            mode = 0
+            commandBuffer = ""
+            Timer1.Enabled = True
+        ElseIf e.KeyCode = Keys.D0 Then
+
+            For i = textArea1.SelectionStart To 0 Step -1
+
+                Try
+                    If textArea1.Text.Substring(i, 1) = vbLf Then
+                        textArea1.SelectionStart = i + 1
+                        Exit Sub
+                    End If
+
+                Catch ex As Exception
+                End Try
+            Next
+            textArea1.SelectionStart = 0
+        ElseIf e.KeyCode = Keys.D4 And e.Shift Then
+            For i = textArea1.SelectionStart To textArea1.TextLength
+                Try
+                    If textArea1.Text.Substring(i, 1) = vbLf Then
+
+                        textArea1.SelectionStart = i - 2
+                        Exit Sub
+
+                    End If
+                Catch ex As Exception
+                End Try
+            Next
+            'テキストの最後の場合
+            textArea1.SelectionStart = textArea1.TextLength - 1
+
+        ElseIf e.KeyCode = Keys.Oem1 Then
+            textArea2.Text += ":"
+            textArea2.SelectionStart += 1
+            textArea2.Focus()
+        ElseIf e.KeyCode = Keys.A And IsKeyLocked(Keys.CapsLock) = False And e.Shift = False And mode = 0 Then
+            textArea1.SelectionStart += 1
+        ElseIf e.KeyCode = Keys.H And IsKeyLocked(Keys.CapsLock) = False And e.Shift = False And mode = 0 Then
+            Try
+                'vimらしい最後に移動できない処理を追加する。
+                If textArea1.Text.Substring(textArea1.SelectionStart - 1, 1) = vbLf Then
+                Else
+                    textArea1.SelectionStart -= 1
+                End If
+            Catch ex As Exception
+            End Try
+        ElseIf e.KeyCode = Keys.L And IsKeyLocked(Keys.CapsLock) = False And e.Shift = False And mode = 0 Then
+            Try
+                'vimらしい最後に移動できない処理を追加する。
+                If textArea1.Text.Substring(textArea1.SelectionStart + 2, 1) = vbLf Then
+                Else
+                    textArea1.SelectionStart += 1
+                End If
+            Catch ex As Exception
+                If textArea1.SelectionStart = textArea1.TextLength - 1 Then
+                Else
+                    textArea1.SelectionStart += 1
+                End If
+            End Try
+        ElseIf e.KeyCode = Keys.K And IsKeyLocked(Keys.CapsLock) = False And e.Shift = False And mode = 0 Then
+            For i = textArea1.SelectionStart To 0 Step -1
+                Try
+                    If textArea1.Text.Substring(i, 1) = vbLf Then
+                        counter += 1
+
+                        If counter = 2 Then
+                            textArea1.SelectionStart = i + caretColumn + 1
+                            counter = 0
+                            Exit For
+                        End If
+
+                    End If
+                Catch ex As Exception
+                End Try
+            Next
+            If counter = 1 Then
+                textArea1.SelectionStart = caretColumn
+            End If
+        ElseIf e.KeyCode = Keys.J And IsKeyLocked(Keys.CapsLock) = False And e.Shift = False And mode = 0 Then
+            For i = textArea1.SelectionStart To textArea1.TextLength
+                Try
+                    If textArea1.Text.Substring(i, 1) = vbLf Then
+
+                        textArea1.SelectionStart = i + caretColumn + 1
+                        Exit For
+
+                    End If
+                Catch ex As Exception
+                End Try
+            Next
+
+        ElseIf e.KeyCode = Keys.X And IsKeyLocked(Keys.CapsLock) = False And e.Shift = False And mode = 0 Then
+            Dim pos As Integer = textArea1.SelectionStart
+            'キャレット位置が最後か最後-1なら
+            If textArea1.SelectionStart = textArea1.TextLength Then
+            ElseIf textArea1.SelectionStart = textArea1.TextLength - 1 Then
+                textArea1.Text = textArea1.Text.Substring(0, textArea1.SelectionStart)
+                textArea1.SelectionStart = pos
+            Else
+                textArea1.Text = textArea1.Text.Substring(0, textArea1.SelectionStart) + textArea1.Text.Substring(textArea1.SelectionStart + 1, textArea1.TextLength - textArea1.SelectionStart - 1)
+                textArea1.SelectionStart = pos
+            End If
+        ElseIf e.KeyCode = Keys.Z And (IsKeyLocked(Keys.CapsLock) = True Or e.Shift = True) And mode = 0 Then
+            commandBuffer += "Z"
+            If commandBuffer = "ZZ" Then
+                '保存して終了
+                writeTextfile()
+                Application.Exit()
+            End If
+        End If
+    End Sub
+
+    Private Sub textArea1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles textArea1.KeyPress
+        If e.KeyChar = "i" And mode = 0 Then
+            mode = 1
+            e.Handled = True
+            Timer1.Enabled = True
+        ElseIf e.KeyChar = "a" And mode = 0 Then
+            mode = 1
+            e.Handled = True
+        ElseIf mode = 1 Then
+            changed = True
+        ElseIf e.KeyChar = "Z" Then
+            e.Handled = True
+        Else
+            commandBuffer = ""
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        caretRow = textArea1.GetLineFromCharIndex(textArea1.SelectionStart)
+        caretColumn = textArea1.SelectionStart - textArea1.GetFirstCharIndexFromLine(caretRow)
+        StatusStrip1.Items(0).Text = "" + caretColumn.ToString() + ":" + caretRow.ToString()
+        If mode = 0 Then
+            StatusStrip1.Items(0).Text = "コマンドモード:(col=" + caretColumn.ToString + ",row=" + caretRow.ToString() + ")" + ":" + "changed=" + changed.ToString
+        ElseIf mode = 1 Then
+            StatusStrip1.Items(0).Text = "入力モード:" + caretColumn.ToString + ",row=" + caretRow.ToString() + ")" + ":" + "changed=" + changed.ToString
+        End If
+
+    End Sub
+
+    Private Sub readTextfile(filename As String)
+
+        Dim sjisEnc As Encoding = Encoding.GetEncoding("Shift_JIS")
+        Dim reader As StreamReader = New StreamReader(filename, sjisEnc)
+        Dim line As String = ""
+        Dim lines As String = ""
+
+        line = reader.ReadLine()
+        lines += line + vbNewLine
+
+        Do While reader.Peek() >= 0
+            line = reader.ReadLine()
+            lines += line + vbNewLine
+        Loop
+
+        textArea1.Text = lines
+
+        reader.Close()
+
+    End Sub
+
+    Private Sub writeTextfile()
+        If filename = "" Then
+        Else
+            Dim sjisEnc As Encoding = Encoding.GetEncoding("Shift_JIS")
+            Dim writer As StreamWriter = New StreamWriter(filename, False, sjisEnc)
+            Dim lines As String() = textArea1.Text.Split(vbNewLine)
+            For Each line In lines
+                writer.Write(line.Replace(vbLf, "") + vbCr + vbLf)
+            Next
+            writer.Close()
+            changed = False
+
+        End If
+    End Sub
+
+    Private Sub textArea2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles textArea2.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            If textArea2.Text = ":q" Then
+                If changed = False Then
+                    Application.Exit()
+                Else
+                    Timer1.Enabled = False
+                    StatusStrip1.Items(0).Text = "保存していません！"
+                    textArea1.Focus()
+                End If
+            ElseIf textArea2.Text = ":q!" Then
+                Application.Exit()
+            ElseIf textArea2.Text = ":w" Then
+                'ファイル書き込み
+                writeTextfile()
+                textArea1.Focus()
+            End If
+            textArea2.Text = ""
+            commandBuffer = ""
+        End If
+    End Sub
+End Class
